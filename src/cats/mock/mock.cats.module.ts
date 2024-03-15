c0a452cc729ba29a6cc2aceed8a5d86f894af8f8
litao <714755132@qq.com>
2024-03-16 01:01:34 +0800

    新增cate service mock

---
+++ src/cats/mock/mock.cats.module.ts
@@ -0,0 +1,13 @@
+import { CatsService } from '../cats.service';
+import { CreateCatDto } from '../deo/create.cat.dto';
+
+export class mockCatsService implements CatsService {
+  constructor(private readonly num: number) {}
+  create(cat: CreateCatDto): { success: boolean } {
+    console.log(cat);
+    console.log('mock 中的 Service');
+    console.log('还能传参', this.num);
+
+    return { success: true };
+  }
+}
